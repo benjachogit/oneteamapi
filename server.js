f0b918984d03c1a7151dfcef2103472aa8ea580b
Jettapat <hennet2@hotmail.com>
2019-07-24 16:44:57 +0700

    first3

--- server.js
+++ server.js
@@ -36,9 +36,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./queries')
+var cors = require('cors')
 const app = express()
 const port = 3000
 
+
+app.use(cors());
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
