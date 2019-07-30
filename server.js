aefdeb40bf3b941729ceb9dd32e243b9e6bd111b
Jettapat <hennet2@hotmail.com>
2019-07-30 14:45:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ app.get('/getpicture/', cors(issue2options), db.getPic);
 app.post('/register/', cors(issue2options),db.register);
 app.get('/getgraph/:id', cors(issue2options), db.getPriceGraph);
 app.post('/insertbasketoffer/', cors(issue2options), db.insertBusketOffer);
+app.get('/productoffer/:id', cors(issue2options), db.getProdById3offer);
   
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
