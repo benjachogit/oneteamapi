870ded4fbfa68d62eaf81a0aa9021e72787f57a3
Jettapat <hennet2@hotmail.com>
2019-07-31 18:12:25 +0700

    Update server.js
    GetGraph,GetUser

--- server.js
+++ server.js
@@ -68,6 +68,8 @@ app.post('/register/', cors(issue2options),db.register);
 app.get('/getgraph/:id', cors(issue2options), db.getPriceGraph);
 app.post('/insertbasketoffer/', cors(issue2options), db.insertBusketOffer);
 app.get('/productoffer/:id', cors(issue2options), db.getProdById3offer);
+app.get('/getgraph/:id', cors(issue2options), db.getGraph);
+app.get('/alluser', cors(issue2options), db.getUser);
   
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
