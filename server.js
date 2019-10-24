57d8fb1f29c90d0d413bf50fc469e01873133512
Jettapat <hennet2@hotmail.com>
2019-10-24 16:20:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -73,60 +73,6 @@ app.get('/alluser', cors(issue2options), db.getUser);
 app.post('/rate/', cors(issue2options),db.rate);
 
 
-// start face api //
-
-// Replace <Subscription Key> with your valid subscription key.
-const subscriptionKey = 'e8a03ab9d5ba41c4af0cb79dde5c4926';
-
-// You must use the same location in your REST call as you used to get your
-// subscription keys. For example, if you got your subscription keys from
-// westus, replace "westcentralus" in the URL below with "westus".
-const uriBase = 'https://southeastasia.api.cognitive.microsoft.com/face/v1.0';
-
-const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg';
-
-
-
-// Request parameters.
-const params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-};
-
-const options = {
-    uri: uriBase,
-    qs: params,
-    body: '{"url": ' + '"' + imageUrl + '"}',
-    headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key' : subscriptionKey
-    }
-};
-
-app.post('/detect/',cors(issue2options),options, (error, response, body) => {
-  if (error) {
-    console.log('Error: ', error);
-    return;
-  }
-  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-  console.log('JSON Response\n');
-  console.log(jsonResponse);
-});
-
-
-// end face api //
-
-
-
-
-
-
-
-
-
-
   
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
