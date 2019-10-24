5ac27513e4db1ab5f1a3040ead699c37d56afd51
Jettapat <hennet2@hotmail.com>
2019-10-24 17:18:48 +0700

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,5 +1,6 @@
 
 const Pool = require('pg').Pool
+const request = require('request');
 
 const pool = new Pool({
   host : 'onedemo.metrosystems.co.th',
@@ -316,6 +317,97 @@ const insertBusketOffer = async (request, response) => {
 
 
 
+// face start //
+
+//initial//
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = '8b1838e13407455daf92a98bd51016ba';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriDetect = 'https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect/';
+
+const imageUrl =
+    'https://noblesamplebot9711.blob.core.windows.net/oneteam/arun_1.PNG';
+
+    // Request parameters.
+const params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+};
+
+const options = {
+    uri: uriDetect,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+// initial end//
+const getMonthly = async (req, res) => {
+  request.post(options, (error, response, body) => {
+    if (error) {
+      console.log('Error: ', error);
+      return;
+    }
+    let jsonResponse = JSON.parse(body);
+    //let res = JSON.stringify(jsonResponse, null, '  ');
+    var date = new Date();
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+  
+    let gender = jsonResponse[0].faceAttributes.gender;
+    let age = jsonResponse[0].faceAttributes.age;
+    console.log('JSON Response\n');
+    console.log("age: " ,age + " , gender: " + gender);
+  
+    var cluster = "";
+    if(gender="female"){
+      if(age>=54 && age <= 80){
+        cluster="Cluster_1";
+      }else if(age>=41 && age <= 53 ){
+        cluster="Cluster_2"
+      }else if(age>=15 && age <= 40 ){
+        cluster="Cluster_3"
+      }
+      else{
+        cluster="Cluster_3"
+      }
+    }if(gender="male"){
+      if(age>=56 && age <= 80){
+        cluster="Cluster_6";
+      }else if(age>=42 && age <= 55 ){
+        cluster="Cluster_5"
+      }else if(age>=15 && age <= 41 ){
+        cluster="Cluster_4"
+      }else{
+        cluster="Cluster_4"
+      }
+    }
+      pool.query('SELECT round_500 FROM  public_b1.cluster_monthly_spent where cluster_group = $1 and Day = $2 and Month = $3;',[cluster,day,month], (error, results) => {
+        if (error) {
+          throw error
+        }
+       // response.status(200).json(results.rows);
+       console.log(day+" "+month+" "+cluster);
+        res.send(results.rows);
+      })
+  });
+
+}
+
+
+// face end //
+
+
+
+
 
 
 
@@ -337,5 +429,6 @@ const insertBusketOffer = async (request, response) => {
     insertBusketOffer,
     getProdById3offer,
     getGraph,
-    getUser
+    getUser,
+    getMonthly
   }
