0ea8c585c8a1b45a703646d2fb7c54733645e9a5
Jettapat <hennet2@hotmail.com>
2019-10-24 17:13:47 +0700

    add code facial

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const bodyParser = require('body-parser')
 const db = require('./queries')
 var app = express()
 var port = process.env.PORT || 3000
+const request = require('request');
 
 app.use(bodyParser.json())
 app.use(
@@ -69,6 +70,97 @@ app.post('/insertbasketoffer/', cors(issue2options), db.insertBusketOffer);
 app.get('/productoffer/:id', cors(issue2options), db.getProdById3offer);
 app.get('/getgraph/:id', cors(issue2options), db.getGraph);
 app.get('/alluser', cors(issue2options), db.getUser);
+
+
+
+
+
+// face start //
+
+//initial//
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = '8b1838e13407455daf92a98bd51016ba';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriDetect = 'https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect/';
+
+const imageUrl =
+    'https://noblesamplebot9711.blob.core.windows.net/oneteam/arun_1.PNG';
+
+    // Request parameters.
+const params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+};
+
+const options = {
+    uri: uriDetect,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+// initial end//
+app.get('/monthly/',cors(issue2options),function(req,res){
+  request.post(options, (error, response, body) => {
+    if (error) {
+      console.log('Error: ', error);
+      return;
+    }
+    let jsonResponse = JSON.parse(body);
+    //let res = JSON.stringify(jsonResponse, null, '  ');
+    var date = new Date();
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+  
+    let gender = jsonResponse[0].faceAttributes.gender;
+    let age = jsonResponse[0].faceAttributes.age;
+    console.log('JSON Response\n');
+    console.log("age: " ,age + " , gender: " + gender);
+  
+    var cluster = "";
+    if(gender="female"){
+      if(age>=54 && age <= 80){
+        cluster="Cluster_1";
+      }else if(age>=41 && age <= 53 ){
+        cluster="Cluster_2"
+      }else if(age>=15 && age <= 40 ){
+        cluster="Cluster_3"
+      }
+      else{
+        cluster="Cluster_3"
+      }
+    }if(gender="male"){
+      if(age>=56 && age <= 80){
+        cluster="Cluster_6";
+      }else if(age>=42 && age <= 55 ){
+        cluster="Cluster_5"
+      }else if(age>=15 && age <= 41 ){
+        cluster="Cluster_4"
+      }else{
+        cluster="Cluster_4"
+      }
+    }
+      pool.query('SELECT round_500 FROM  public_b1.cluster_monthly_spent where cluster_group = $1 and Day = $2 and Month = $3;',[cluster,day,month], (error, results) => {
+        if (error) {
+          throw error
+        }
+       // response.status(200).json(results.rows);
+       console.log(day+" "+month+" "+cluster);
+        res.send(results.rows);
+      })
+  });
+
+});
+
+
+// face end //
 
 
   
